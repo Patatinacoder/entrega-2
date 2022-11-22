85602488d78fe83037ef080e2f54b1073aff8b24
Patatinacoder <105814556+Patatinacoder@users.noreply.github.com>
2022-11-22 18:14:33 -0300

    actualizando

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Ad from "../../components/Ad";
 import MovieCard from "../../components/Item";
 import products from "../../data/products";
 import styles from "./MoviesGrid.module.css";
 
-export function MoviesGrid() {
-    const [movies, setMovies] = useState([]);
-    console.log("🚀 ~ file: index.jsx ~ line 10 ~ MoviesGrid ~ movies", movies);
 
+
+export function MoviesGrid(){
+const [movies, setMovies]=useState([]);
+const {categoryId} = useParams();
     useEffect(() => {
         const promesa = new Promise((resolve, reject) => {
             resolve(products);
         });
-        promesa.then((data) => setMovies(data));
-    }, []);
+        if(!categoryId) promesa.then((data)=> setMovies(data))
+        else{
+          promesa.then((data)=>
+          setMovies(
+            data.filter((product)=>product.category===categoryId)
+          )
+    );
+          }
+        }, [categoryId]);
+        
+       
 
     return (
         <ul className={styles.moviesGrid}>
@@ -22,7 +33,7 @@ export function MoviesGrid() {
             ))}
         </ul>
     );
-}
+            }
 <Ad>
     <h1>Este es un anuncio molesto</h1>
     <button>cerrar anuncio</button>
