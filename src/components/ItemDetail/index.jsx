c6eb6736791739a4d1d0f31b49bb52e239da980c
Patatinacoder <105814556+Patatinacoder@users.noreply.github.com>
2022-12-03 02:05:33 -0300

    we're close

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 //import styles from "./ItemDetail.modules.css"
 import "../Item/index"
+import ItemCount from '../ItemCount';
 import styles from "./styles.css"
 
 const ItemDetail = ({movie})=>{
+    const [quantityItemDetail, setQuantityItemDetail]= useState(0)
+    const confirmPurchase = (quantity) =>{
+        console.log(quantity);
+        setQuantityItemDetail(quantity)
+        
+    }
     console.log(movie);
     return(
-        <div className="detailsContainer">
-            <div className="col">
+        <div className="col">
             <img  className="movieImage"src={movie.poster} alt="details" />
-            <h1 className="movie.details">{movie.name}</h1>
+            <h1 className="firstItem">{movie.name}</h1>
             <h2 className="movie.details">{movie.description}</h2>
             <button className={styles.button}>Button</button>
-            </div>
-            
-        </div>
+            {quantityItemDetail ? 
+            <button>Go cart</button>
+            :
+           
+          <ItemCount onAdd={confirmPurchase} initial={1} stock ={20} />
+        }
+             </div>
     )
 }
 
